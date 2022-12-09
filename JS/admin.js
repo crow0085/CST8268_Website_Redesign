9c4fef1840fffc150c002d3fa2e8588ce29b07b1
crow0085 <crow0085@algonquinlive.com>
2022-12-08 23:11:56 -0500

    added put functionality with api calls

--- JS/admin.js
+++ JS/admin.js
@@ -1,32 +1,58 @@
-var getUrl = "http://localhost:5000/websiteredesign";
+var apiUrl = "http://localhost:5000/websiteredesign";
 var peopleJson;
+var imageNames;
+var currentSelected = -1;
 
 $(document).ready(async function () {
+    $('#lblConfirmation').hide();
     getJsonData();
+    getImageData();
 });
 
-function getJsonData(){
+function getJsonData() {
     $.ajax({
         type: 'GET',
-        url: getUrl,
-        headers: { 'Access-Control-Allow-Origin': '*'},
+        url: apiUrl,
+        headers: { 'Access-Control-Allow-Origin': '*' },
         dataType: 'json',
         success: function (returnData) {
             //alert('success');
-            if (returnData !== null)
-            {
-                $.each(returnData, function (index, value)
-                {
+            if (returnData !== null) {
+                $('#drp-person').empty();
+                if (currentSelected == -1)
+                    $('#drp-person').append("<option value='-1'>select...</option>");
+                $.each(returnData, function (index, value) {
                     $('#drp-person').append("<option value='" + index + "'>" + value.name + "</option>");
-                    $('#drp-img').append("<option value='" + index + "'>" + value.image + "</option>");
                 });
-                
-                console.log(returnData);
+                $('#drp-person').val(currentSelected);
+                if (currentSelected != -1)
+                    console.log(returnData);
                 peopleJson = returnData;
             }
         },
-        error: function (event, request, settings)
-        {
+        error: function (event, request, settings) {
+            alert('AjaxError' + ' : ' + settings);
+        }
+    });
+}
+
+function getImageData() {
+    $.ajax({
+        type: 'GET',
+        url: apiUrl + "/GetImageFileNames",
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        dataType: 'json',
+        success: function (returnData) {
+            //alert('success');
+            if (returnData !== null) {
+                $.each(returnData, function (index, value) {
+                    $('#drp-img').append("<option value='" + value + "'>" + value + "</option>");
+                });
+                console.log(returnData);
+                imageNames = returnData;
+            }
+        },
+        error: function (event, request, settings) {
             alert('AjaxError' + ' : ' + settings);
         }
     });
@@ -35,16 +61,45 @@ function getJsonData(){
 $('#drp-person').change(function () {
     $("#drp-person option[value='-1']").remove();
     $("#drp-img option[value='-1']").remove();
-    var index = this.value;
-    $('#name').val(peopleJson[index].name);
-    $('#team').val(peopleJson[index].team);
-    $('#description').val(peopleJson[index].description);
-    $('#drp-img').val(index);
-    $('#drp-img-prev').attr('src', getUrl + "/" + peopleJson[index].image);
+    currentSelected = this.value;
+    $('#name').val(peopleJson[currentSelected].name);
+    $('#team').val(peopleJson[currentSelected].team);
+    $('#description').val(peopleJson[currentSelected].description);
+    $('#drp-img').val(peopleJson[currentSelected].image);
+    $('#drp-img-prev').attr('src', apiUrl + "/" + $('#drp-img option:selected').text());
 });
 
-$('#drp-img').change(function (e) { 
+$('#drp-img').change(function (e) {
     $("#drp-img option[value='-1']").remove();
-    var index = this.value;
-    $('#drp-img-prev').attr('src', getUrl + "/" + peopleJson[index].image);
+    $('#drp-img-prev').attr('src', apiUrl + "/" + $('#drp-img option:selected').text());
+});
+
+$('#save').click(async function (e) {
+
+    var person = {
+        name: $('#name').val(),
+        image: $('#drp-img option:selected').text(),
+        team: $('#team').val(),
+        description: $('#description').val(),
+        id: peopleJson[$("#drp-person").val()].id
+    }
+    console.log(person);
+    await $.ajax({
+        url: apiUrl,
+        type: 'PUT',
+        contentType: "application/json",
+        data: JSON.stringify(person),
+        async: true,
+        success: function () {
+            getJsonData();
+            $('#lblConfirmation').show();
+            $('#lblConfirmation').html("Successfully edited " + $('#name').val());
+        },
+        error: function (event, request, settings) {
+            console.log(settings);
+            console.log(event);
+            console.log(request);
+        }
+    });
+
 });
